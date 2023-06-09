7d551065ecf522a6fa584573cff57a4995fb1d0a
itmozahidul <itmozahidul@gmail.com>
2023-06-09 14:20:17 +0200

    update

---
+++ back/demo/src/main/java/DTO/Location.java
@@ -0,0 +1,11 @@
+package DTO;
+
+public class Location {
+	private String[] data;
+
+	public Location(String[] loc) {
+		// TODO Auto-generated constructor stub
+		this.data = loc;
+	}
+
+}
---
+++ back/demo/src/main/java/com/shk/ConnectMe/Model/Chat.java
@@ -0,0 +1,9 @@
+package com.shk.ConnectMe.Model;
+
+public class Chat {
+
+	public Chat() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
